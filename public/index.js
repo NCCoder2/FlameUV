343b9628138f65679f57e94fea0f98e5fb4fec55
NCCoder2 <133831293+NCCoder2@users.noreply.github.com>
2023-06-01 16:59:51 +0000

    as

--- public/index.js
+++ public/index.js
@@ -19,3 +19,21 @@ function isUrl(val = '') {
   if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
   return false;
 };
+
+
+function openURL(url) {
+    window.navigator.serviceWorker
+    .register("./sw.js", {
+      scope: __uv$config.prefix,
+    })
+    .then(() => {
+      var selectedSearchEngine = localStorage.getItem("selectedSearchEngine");
+    var searchEngine = input.getAttribute("data-search-engine");
+    var url = input.value.trim();
+    if (!isUrl(url)) url = (searchEngine || selectedSearchEngine) + url;
+    else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+
+    window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+  
+    });
+};
